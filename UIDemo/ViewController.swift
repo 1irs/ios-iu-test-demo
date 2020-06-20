60f7cbf49b22783996b0ae647af82681d2811ac7
Vladimir Obrizan <volodymyr.obrizan@gmail.com>
2020-06-20 08:13:36 +0300

    Initial Commit

---
+++ UIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIDemo
+//
+//  Created by Vladimir Obrizan on 20.06.2020.
+//  Copyright © 2020 First Institute of Reliable Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
